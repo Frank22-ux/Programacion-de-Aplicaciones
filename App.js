d61e0b588b2dc30650ceb13b6ab83bcf0c76de49
Frank22-ux <jose22.quezada@gmail.com>
2025-05-08 11:53:11 -0500

    diseños

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>¡Bienvenido a mi App!</Text>
-      <Text style={styles.subtitle}>Edita App.js para comenzar</Text>
-
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Empezar</Text>
-      </TouchableOpacity>
-
-      <StatusBar style="light" />
+      <Text>Hola soy Goku!!!</Text>
+      <Image style={styles.imagen} source={{uri:"https://play-lh.googleusercontent.com/3wPaD-PGK0f0RlM3sqH0rFEV0gLwhAGDRaIK8DcDvK8bExY6pR8INE6F0EjuHIIp5Whx"}}/> 
+      <TextInput style={styles.TextInput} keyboardType='numeric' placeholder='Ingrese su peso en kg: ' />
+      <TextInput style={styles.TextInput} keyboardType='numeric' placeholder='Ingrese su altura en cm: ' />    
+      <Button title='Calcular' onPress={() => {
+        Alert.alert("Hola soy Goku!!!", "Es momento de entrenar");
+      }} />
     </View>
   );
 }
@@ -19,38 +17,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1e1e2f',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
   },
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: '#ffffff',
-    marginBottom: 10,
+  imagen: {
+    width: 200,
+    height: 200,
   },
-  subtitle: {
-    fontSize: 16,
-    color: '#aaa',
-    marginBottom: 30,
-    textAlign: 'center',
-  },
-  button: {
-    backgroundColor: '#5e60ce',
-    paddingVertical: 12,
-    paddingHorizontal: 24,
+
+  TextInput:{
+    height: 50,
+    borderColor: 'gray',
     borderRadius: 10,
-    elevation: 3,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: '600',
-  },
+    borderWidth: 3,
+    marginBottom: 10,
+    width: 200,
+    paddingLeft: 10,
+  }
+
 });
 
